955d95300c15eb1dd63de767e30d08690c8ee30a
Namase7 <nachomartnezserrano@gmail.com>
2024-10-07 19:11:59 +0200

    Ejercicio clase
    Finalizado

--- ganador.sh
+++ ganador.sh
@@ -4,3 +4,10 @@ echo "Hola $1"
 
 mkdir -p $1
 touch $1/ganador.txt
+
+minutos=$(date +"%M")
+if (( minutos % 2 == 0 )); then
+    echo "Has ganado">>$1/ganador.txt
+else
+    echo "No has ganado">>$1/ganador.txt
+fi
